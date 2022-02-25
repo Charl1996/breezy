f5f067af8e49e14a11e9022017e7ebea84279341
Charl Smit <csmit@dimagi.com>
2022-02-25 10:32:22 +0200

    Add secrets fiel

--- code/configs.py
+++ code/configs.py
@@ -1,3 +1,9 @@
+from secrets import (
+    RESPONDIO_API_URL,
+    RESPONDIO_API_TOKEN,
+    BREEZE_API_URL,
+    BREEZE_API_KEY,
+)
 
 DEFAULT_EMAIL = 'charlsmit96@gmail.com'
 
@@ -136,11 +142,6 @@ API_FIELD_MAPPINGS = {
     },
 }
 
-RESPONDIO_API_URL = 'https://app.respond.io/api/v1/'
-RESPONDIO_API_TOKEN = 'f5b39f52ba96fdacf47f0b1fe145cf68c5ffcaf8fe5108cbab7c0e18b576ebc51342ace4f3ebac2eb89f268bf9c5c8e1176886778e302e8e24976acff77544d1'
-
-BREEZE_API_URL = 'https://gesinskerk.breezechms.com/api/'
-BREEZE_API_KEY = 'dd6bdc926910babb9e745163ee11b4d0'
 
 BREEZE_TO_CSV_HEADER_CONVERTERS = {
     'id': 'Breeze ID',
